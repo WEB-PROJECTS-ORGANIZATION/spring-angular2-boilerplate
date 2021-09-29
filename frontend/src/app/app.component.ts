f3a9f83853b8123e10377236438ff4e9234a411a
DusanStevic <stevicdule@gmail.com>
2021-09-29 23:05:24 +0200

    ngx-toastr setup

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private toastr: ToastrService) {}
+
+  // uccess, error, info, warning take (message, title, ToastConfig) pass an options object to replace any default option.
+  showSuccess() {
+    // (message, title)
+    this.toastr.success('msg success!', 'title success!');
+  }
+
+  showError() {
+    // (message, title)
+    this.toastr.error('msg error!', 'title error!');
+  }
+
+  showInfo() {
+    // (message, title)
+    this.toastr.info('msg info!', 'Title info!');
+  }
+
+  showWarning() {
+    // (message, title)
+    this.toastr.warning('msg warning!', 'title warning!');
+  }
 }
